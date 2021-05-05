17769ce1bd799828856e74290233e01eb60e25fa
steve <gichungasteve6@gmail.com>
2021-05-05 23:16:32 +0000

    initial project configuration

---
+++ resources/js/components/TaskForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">TaskForm Component</div>
+
+                    <div class="card-body">
+                        <form @submit.prevent="addTask">
+                          <input type="text" name="title" placeholder="Enter Title" class="form-control">
+                          <input type="submit" value="Submit" class="btn btn-primary form-control btn-sm d-inline-block p-2">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+
+<style scoped>
+.container{
+  margin-top: 20px;
+}
+</style>
